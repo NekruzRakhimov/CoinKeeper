bc70152f0fa360503cfe3fc8934065ce2739f08f
Andrey Koval <157694430+AndreyAko57@users.noreply.github.com>
2024-01-27 23:32:52 +0500

    Update connection.py

--- connection.py
+++ connection.py
@@ -1,17 +1,14 @@
-import psycopg2
-
 from security import dbname_app, user_app, password_app, host_app, port_app
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
 
 
-# connect db
-# Подключаем базу для дальнейшей работы
-conn = psycopg2.connect(dbname=dbname_app,
-                        user=user_app,
-                        password=password_app,
-                        host=host_app,
-                        port=port_app)
-conn.set_client_encoding('UTF8')
-# conn.autocommit = True
-cursor = conn.cursor()
+# Атрибут подключения для движка SQLAlchemy.
+DATABASE_URL = f'postgresql+psycopg2://{
+    user_app}:{password_app}@{host_app}:{port_app}/{dbname_app}'
 
-##############################
+# Создаём движок SQLAlchemy.
+engine = create_engine(DATABASE_URL, echo=True)
+
+# Создаем класс последующих сессий, на основе которого будут создаваться разовые экземпляры для разовых подключений.
+Session = sessionmaker(bind=engine)
