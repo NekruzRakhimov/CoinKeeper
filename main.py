ab89f8ed4f0e7ee6d825a00c3b8a1a055de87795
AndreyAko57 <andrey57@mail.ru>
2024-01-27 20:10:17 +0500

    [feature] Mod main.py file...

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello World")
+from flask import Flask
+from routes import app as routes_app
+
+# Create app
+app = Flask(__name__)
+app.register_blueprint(routes_app)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=7000)
