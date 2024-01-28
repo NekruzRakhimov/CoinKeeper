5e7ce3f5f6dfdc8a7fe1efec6231602baba3a618
AndreyAko57 <andrey57@mail.ru>
2024-01-28 14:24:30 +0500

    [refactor] Modify routes file...

--- routes.py
+++ routes.py
@@ -1,4 +1,3 @@
-import random
 import repository
 from models import Accounts
 from flask import jsonify, request, render_template, Blueprint
@@ -10,3 +9,40 @@ app = Blueprint('routes', __name__)
 @app.route('/', methods=['GET'])
 def index():
     return {"Status": "Up"}, 200
+
+
+# Получение списка балансов
+@app.route('/balances', methods=['GET'])
+def get_balances():
+    serialized_balances = []
+    # balances = repository.get_balances()
+    balances = [{'id': 1,
+                 'title': 'Мой баланс',
+                 'balance': 10000},
+                {'id': 2,
+                 'title': 'Мой баланс2',
+                 'balance': 20000}]
+
+    for balance in balances:
+        # dict_balance = balance.__dict__
+        # del dict_balance['_sa_intence_state']
+        serialized_balances.append(balance)
+    return {'Balances': serialized_balances}, 200
+
+
+# Получение списка расходов
+@app.route('/incomes', methods=['GET'])
+def get_incomes():
+    serialized_incomes = []
+    # all_incomes = repository.get_incomes()
+    all_incomes = [{'id': 1,
+                    'title': 'Мои траты',
+                    'amount': 990},
+                   {'id': 2,
+                    'title': 'Мои траты2',
+                    'amount': 1990}]
+    for income in all_incomes:
+        # dict_incomes = income.__dict__
+        # del dict_incomes['_sa_intence_state']
+        serialized_incomes.append(income)
+    return {'Incomes': serialized_incomes}, 200
