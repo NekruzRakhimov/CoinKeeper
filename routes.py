a763d4fe31e71396bd3f1d5de99ced5b4bfeb5cf
Nikolai-Nikolin <nikolay.nikolin@gmail.com>
2024-01-28 21:48:43 +0500

    [refactor] add two routes

--- routes.py
+++ routes.py
@@ -43,3 +43,23 @@ def get_incomes():
         # del dict_incomes['_sa_intence_state']
         serialized_incomes.append(income)
     return {'Incomes': serialized_incomes}, 200
+
+
+# Пополнение баланса
+@app.route('/top_up_balance/<int:id>', methods=['PATCH'])
+def top_up_balance(_id):
+    if not isinstance(_id, int):
+        return jsonify({'Error': 'Invalid customer ID format.'}), 400
+    data = request.get_json()
+    balance = float(data['balance'])
+    # repository.top_up_balance(_id, balance)
+    return {"status": "balance updated"}, 200
+
+
+# Оплатить расход
+@app.route('/expenses/<int:id>', methods=['PATCH'])
+def pay_expenses(_id):
+    if not isinstance(_id, int):
+        return jsonify({'Error': 'Invalid customer ID format.'}), 400
+    # repository.pay_expenses(_id)
+    return {"status": "balance withdrawn"}, 200
