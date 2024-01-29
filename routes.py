bc5266b7600ce15117ec8afd3da45d3008d67bab
AndreyAko57 <andrey57@mail.ru>
2024-01-29 09:17:58 +0500

    [refactor] Modify routes file...

--- routes.py
+++ routes.py
@@ -11,23 +11,14 @@ def index():
     return {"Status": "Up"}, 200
 
 
-# Получение списка балансов
-@app.route('/balances', methods=['GET'])
-def get_balances():
-    serialized_balances = []
-    # balances = repository.get_balances()
-    balances = [{'id': 1,
-                 'title': 'Мой баланс',
-                 'balance': 10000},
-                {'id': 2,
-                 'title': 'Мой баланс2',
-                 'balance': 20000}]
-
-    for balance in balances:
-        # dict_balance = balance.__dict__
-        # del dict_balance['_sa_intence_state']
-        serialized_balances.append(balance)
-    return {'Balances': serialized_balances}, 200
+# Получение списка категорий
+@app.route('/categories', methods=['GET'])
+def categories_dict():
+    serialized_categories = []
+    categories = repository.categories_dict()
+    for category in categories:
+        serialized_categories.append(category)
+    return {'Categories': serialized_categories}, 200
 
 
 # Получение списка расходов
@@ -46,3 +37,23 @@ def get_incomes():
         # del dict_incomes['_sa_intence_state']
         serialized_incomes.append(income)
     return {'Incomes': serialized_incomes}, 200
+
+
+# Пополнение баланса
+@app.route('/top_up_balance/<int:id>', methods=['PATCH'])
+def top_up_balance(_id):
+    if not isinstance(_id, int):
+        return jsonify({'Error': 'Invalid customer ID format.'}), 400
+    data = request.get_json()
+    balance = float(data['balance'])
+    # repository.top_up_balance(_id, balance)
+    return {"status": "balance updated"}, 200
+
+
+# Оплатить расход
+@app.route('/expenses/<int:id>', methods=['PATCH'])
+def pay_expenses(_id):
+    if not isinstance(_id, int):
+        return jsonify({'Error': 'Invalid customer ID format.'}), 400
+    # repository.pay_expenses(_id)
+    return {"status": "balance withdrawn"}, 200
