afd90f81953ce0e7ffc37c1382c849792737cf30
AndreyAko57 <andrey57@mail.ru>
2024-01-30 13:10:20 +0500

    [refactor] Modify routes file...

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from routes import app as routes_app
 
+
 # Create app
 app = Flask(__name__)
 app.register_blueprint(routes_app)
 
 
+# Run if Point of entry
 if __name__ == '__main__':
     app.run(debug=True, port=7000)
